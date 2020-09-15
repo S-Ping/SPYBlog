ef36fe98fecb48546a7d0ba95e4fefaf5e6cfdfe
S-Ping <su752143565@163.com>
2020-09-15 17:07:30 +0800

    更改端口

--- spy_blog/run.py
+++ spy_blog/run.py
@@ -11,4 +11,4 @@ app = create_app(os.getenv('FLASK_ENV', 'development'))
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8889, use_reloader=False)
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=19531)
